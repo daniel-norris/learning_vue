0d703920fd1f242b3aa6fe31539361f81cfbc251
Daniel Norris <dan.norris@hotmail.com>
2020-08-18 21:17:18 +0100

    created a todo component with props

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ var app2 = new Vue({
         message: 'You loaded this page on ' + new Date().toLocaleString()
     }
 })
+
+Vue.component('todo-item', {
+    template: '<li>{{todo.text}}</li>',
+    props: ['todo'],
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'asdfasdf' },
+            { id: 2, text: 'asdfasdfasdfas' },
+        ]
+    }
+})
+
