11589b970ec759dbf878ce9e528502d9f1e59c50
Daniel Norris <dan.norris@hotmail.com>
2020-08-19 00:04:32 +0100

    added anchor and image tags with attribute binding

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const app = new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
-});
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
+        product: 'Socks',
+        description: 'Socks are socks.',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        imageAlt: 'alt image text',
+        url: 'www.danielnorris.co.uk',
     }
 })
-
-Vue.component('todo-item', {
-    template: '<li>{{todo.text}}</li>',
-    props: ['todo'],
-})
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        groceryList: [
-            { id: 0, text: 'Vegetables' },
-            { id: 1, text: 'asdfasdf' },
-            { id: 2, text: 'asdfasdfasdfas' },
-        ]
-    }
-})
-
